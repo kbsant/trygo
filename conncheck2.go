ec70c451d97e0ef5a0c418c3fa511a7e73ed6962
kbsant <keansant@gmail.com>
2020-02-26 14:43:14 +0900

    wip algorithms - connection checker

--- conncheck2.go
+++ conncheck2.go
@@ -1,17 +1,15 @@
-package conncheck2
+package main
 
 import (
 	"fmt"
 )
 
 // Initialize the connection map.
-// Node zero is a virtual node, representing leaf nodes (non-membership).
-// Node max + 1 is a virtual node, representing all nodes.
-// Initially, every node has its own group since it isn't connected.
-// So every node's group is zero.
+// A node is represented by a nonzero integer, and its entry in the nodes array
+// is the node it is connected to.
+// Zero represents the unconnected node, so initially, every entry is zero
 func initialize(max int) []int {
-	nodes := make([]int, max+2)
-	nodes[max+1] = max + 1
+	nodes := make([]int, max+1)
 	return nodes
 }
 
@@ -19,21 +17,18 @@ func initialize(max int) []int {
 // Group id 0 is unconnected, so skip those.
 // If nodes or their ancestors have the same group, they are connected.
 // If nodes are connected by their ancestors, connect them directly.
+// TODO speed this up, since some logic is duplicated between merge and connected.
 func connected(nodes []int, a int, b int) bool {
-	i, j := a, b
+	i, j := sort2(a, b)
 	for {
-		left, right := nodes[i], nodes[j]
-		if left == 0 || right == 0 {
+		if i == 0 {
 			return false
 		}
-		if left == right {
+		if i == j {
+			merge(nodes, a, b)
 			return true
 		}
-		upleft, upright := nodes[left], nodes[right]
-		nodes[i] = upleft
-		i = upleft
-		nodes[j] = upright
-		j = upright
+		i, j = sort2(i, nodes[j])
 	}
 	fmt.Errorf("Error checking connection between %v and %v\n", a, b)
 	return false
@@ -47,9 +42,16 @@ func sort2(a, b int) (int, int) {
 	}
 }
 
-// Merge 2 nodes by assigning them to the same group.
+// Link the second node to the first, or its ancestor if present.
 func merge(nodes []int, a int, b int) {
 	lo, hi := sort2(a, b)
+	for {
+		parent := nodes[lo]
+		if parent == 0 {
+			break
+		}
+		lo = parent
+	}
 	nodes[hi] = lo
 }
 
@@ -62,19 +64,31 @@ func main() {
 		return connected(nodes, a, b)
 	}
 	fmt.Printf("Are %v and %v connected? %v\n", 2, 5, nodesConnected(2, 5))
+	fmt.Printf("Are %v and %v connected? %v\n", 6, 4, nodesConnected(6, 4))
 	fmt.Printf("Are %v and %v connected? %v\n", 5, 7, nodesConnected(5, 7))
+	fmt.Printf("Are %v and %v connected? %v\n", 5, 6, nodesConnected(5, 6))
 	fmt.Printf("Are %v and %v connected? %v\n", 2, 7, nodesConnected(2, 7))
+	fmt.Printf("Are %v and %v connected? %v\n", 2, 4, nodesConnected(2, 4))
 
 	fmt.Println("Merge 5 and 7.")
+	fmt.Println("Merge 6 and 4.")
 	merge(nodes, 5, 7)
+	merge(nodes, 6, 4)
 	fmt.Printf("Are %v and %v connected? %v\n", 2, 5, nodesConnected(2, 5))
+	fmt.Printf("Are %v and %v connected? %v\n", 6, 4, nodesConnected(6, 4))
 	fmt.Printf("Are %v and %v connected? %v\n", 5, 7, nodesConnected(5, 7))
+	fmt.Printf("Are %v and %v connected? %v\n", 5, 6, nodesConnected(5, 6))
 	fmt.Printf("Are %v and %v connected? %v\n", 2, 7, nodesConnected(2, 7))
+	fmt.Printf("Are %v and %v connected? %v\n", 2, 4, nodesConnected(2, 4))
 
 	fmt.Println("Merge 5 and 2.")
 	merge(nodes, 5, 2)
 	fmt.Printf("Are %v and %v connected? %v\n", 2, 5, nodesConnected(2, 5))
+	fmt.Printf("Are %v and %v connected? %v\n", 6, 4, nodesConnected(6, 4))
 	fmt.Printf("Are %v and %v connected? %v\n", 5, 7, nodesConnected(5, 7))
+	fmt.Printf("Are %v and %v connected? %v\n", 5, 6, nodesConnected(5, 6))
 	fmt.Printf("Are %v and %v connected? %v\n", 2, 7, nodesConnected(2, 7))
+	fmt.Printf("Are %v and %v connected? %v\n", 2, 4, nodesConnected(2, 4))
 
+	fmt.Println("end with ", nodes)
 }
