6c2ab5713e53713680732f40877674fbbc9ac5f5
kbsant <keansant@gmail.com>
2020-02-27 16:28:18 +0900

    comment on connection checker interface

--- conncheck/types.go
+++ conncheck/types.go
@@ -1,5 +1,7 @@
 package conncheck
 
+// Since there are multiple implementations of the connection checker,
+// define an interface
 type Nodes interface {
 	Connected(a, b int) bool
 	Merge(a, b int)
